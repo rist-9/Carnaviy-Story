e5ad6c7003ea2da8ae21c8c252e455d7b65e3e05
rist-9 <yasu.sss9@gmail.com>
2020-04-28 12:47:54 +0000

    fix-view

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,6 @@ module CarnaviyStory
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    unless Rails.env.production?
     config.web_console.whitelisted_ips = '10.0.2.2'
   end
 end
