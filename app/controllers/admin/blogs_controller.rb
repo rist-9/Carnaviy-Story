8bfa4b2e3ffbd934a19a15fd6ddc28e85742ccca
rist-9 <yasu.sss9@gmail.com>
2020-05-11 02:54:13 +0000

    fix-visual

--- app/controllers/admin/blogs_controller.rb
+++ app/controllers/admin/blogs_controller.rb
@@ -6,4 +6,33 @@ class Admin::BlogsController < ApplicationController
     @blogs = Blog.all.page(params[:page]).per(5)
   end
 
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+
+  def new
+    @blog = Blog.new
+  end
+
+  def update
+    @blog = Blog.new
+    if @blog.update(blog_params)
+      redirect_to admin_blogs_path(@blog)
+     else
+       @user = current_user
+       render 'admin/blogs/index'
+    end
+  end
+
+  def destroy
+      blog = Blog.find(params[:id])
+      blog.destroy
+      redirect_to admin_blogs_path
+  end
+
+  private
+    def blog_params
+      params.require(:blog).permit(:title,:introduction,:image,:genres_id)
+    end
+
 end
